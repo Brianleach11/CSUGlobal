3aa620e195f23f9672610d6b049c7a3c0e156b18
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-08-16 16:07:26 -0400

    Add files via upload

---
+++ mod1_problem1.py
@@ -0,0 +1,11 @@
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum: ", addition(num1, num2))
+print("Difference: ", subtraction(num1, num2))
---
+++ mod1_problem2.py
@@ -0,0 +1,11 @@
+def muliplication(x, y):
+    return x * y
+
+def division(x, y):
+    return x / y
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Product: ", muliplication(num1, num2))
+print("Quotient: ", division(num1, num2))
